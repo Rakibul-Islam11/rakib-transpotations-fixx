952f9c211f49350b43c0a941d64b88ac58c91ce8
rakib <kathgolap909@gmail.com>
2024-12-31 16:27:59 +0600

    ki

--- src/all-components/Home-page/inner-pages/LocationSlec.jsx
+++ src/all-components/Home-page/inner-pages/LocationSlec.jsx
@@ -53,6 +53,7 @@ const LocationSlec = ({ reciveApiData }) => {
                             minDate: "today", // আজকের দিন থেকে শুরু
                             maxDate: new Date().fp_incr(10), // সর্বোচ্চ ১০ দিন পর্যন্ত নির্বাচন করা যাবে
                             allowInput: true, // ইনপুটের placeholder দেখাবে
+                            disableMobile: true, // মোবাইলে নেটিভ পিকার ডিজেবল করবে
                         }}
                         value={date || undefined} // তারিখ স্টেট থেকে দেখাবে
                         onChange={dateHandler} // হ্যান্ডলার কল হবে
@@ -60,6 +61,7 @@ const LocationSlec = ({ reciveApiData }) => {
                         placeholder="Choose the date" // প্লেসহোল্ডার
                     />
                 </div>
+
 
 
                 {/* Search Button */}
